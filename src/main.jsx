6d62ec2853070d09dc24d32bd7f50c614575ddd2
Arjun cv <arjun7180@gmail.com>
2024-07-23 23:49:54 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Home from "./App/Main/Home/Home.jsx";
