eb8c07b791be8d0f41f75d910d6de9ab307ffc06
Arjun cv <arjun7180@gmail.com>
2024-07-31 22:57:13 +0530

    bug

--- src/App/Main/Home/HomeSectionThree/SlidingLogos.jsx
+++ src/App/Main/Home/HomeSectionThree/SlidingLogos.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 
 function SlidingLogos() {
   useEffect(() => {
@@ -8,31 +8,36 @@ function SlidingLogos() {
       document.querySelector(".logo-slider").appendChild(copy);
     }
   }, []);
+
   return (
-    <div className="logo-slider">
-      <div className="logos-slide">
-        <img src="./images/placeholderLogo.svg" />
-        <img src="./images/placeholderLogo.svg" />
-        <img src="./images/placeholderLogo.svg" />
-        <img src="./images/placeholderLogo.svg" />
-        <img src="./images/placeholderLogo.svg" />
-        <img src="./images/placeholderLogo.svg" />
+    <div classNameName="logo-slider">
+      <div className="logo-slider">
+        <div className="logos-slide">
+          <img src="https://i.ibb.co/Vv5rkDK/api-partner-verzon.png" />
+          <img src="https://i.ibb.co/J399KDg/api-partner-netflix.png" />
+          <img src="https://i.ibb.co/TKT0F5L/api-partner-yelp.png" />
+          <img src="https://i.ibb.co/HG6KR89/api-partner-adobe.png" />
+          <img src="https://i.ibb.co/hMSJ1sg/api-partner-ring.png" />
+          <img src="https://i.ibb.co/4RWQcMS/api-partner-nespresso.png" />
+        </div>
+        <div className="logos-slide">
+          <img src="https://i.ibb.co/Vv5rkDK/api-partner-verzon.png" />
+          <img src="https://i.ibb.co/J399KDg/api-partner-netflix.png" />
+          <img src="https://i.ibb.co/TKT0F5L/api-partner-yelp.png" />
+          <img src="https://i.ibb.co/HG6KR89/api-partner-adobe.png" />
+          <img src="https://i.ibb.co/hMSJ1sg/api-partner-ring.png" />
+          <img src="https://i.ibb.co/4RWQcMS/api-partner-nespresso.png" />
+        </div>
+        <div className="logos-slide">
+          <img src="https://i.ibb.co/Vv5rkDK/api-partner-verzon.png" />
+          <img src="https://i.ibb.co/J399KDg/api-partner-netflix.png" />
+          <img src="https://i.ibb.co/TKT0F5L/api-partner-yelp.png" />
+          <img src="https://i.ibb.co/HG6KR89/api-partner-adobe.png" />
+          <img src="https://i.ibb.co/hMSJ1sg/api-partner-ring.png" />
+          <img src="https://i.ibb.co/4RWQcMS/api-partner-nespresso.png" />
+        </div>
       </div>
     </div>
   );
 }
-
 export default SlidingLogos;
-
-{
-  /* <img src="https://i.ibb.co/Vv5rkDK/api-partner-verzon.png" />
-  <img src="https://i.ibb.co/J399KDg/api-partner-netflix.png" />
-  <img src="https://i.ibb.co/TKT0F5L/api-partner-yelp.png" />
-  <img src="https://i.ibb.co/HG6KR89/api-partner-adobe.png" />
-  <img src="https://i.ibb.co/hMSJ1sg/api-partner-ring.png" />
-  <img src="https://i.ibb.co/4RWQcMS/api-partner-nespresso.png" /> */
-}
-
-{
-  /* <img src="./images/placeholderLogo.svg" /> */
-}
