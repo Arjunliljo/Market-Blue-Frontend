79a920375fe8f18b1f82e3cfdd9ae20b9facd559
Arjun cv <arjun7180@gmail.com>
2024-08-06 18:57:30 +0530

    some adjustments

--- src/App/Main/Home/HomeSectionSeven/HomeSectionSeven.jsx
+++ src/App/Main/Home/HomeSectionSeven/HomeSectionSeven.jsx
@@ -3,13 +3,30 @@ import LeftSideTextBox from "./LeftSideTextBox";
 import RightSideTextBox from "./RightSideTextBox";
 import useObserver from "../../../Hooks/useObserver";
 import { setIsSectionSeven } from "../../../../Global/Events/eventSlice";
+import { useSelector } from "react-redux";
+import { Parallax } from "react-scroll-parallax";
 
 function HomeSectionSeven() {
   const targetRef = useRef(null);
 
-  useObserver(targetRef, setIsSectionSeven, 0);
+  useObserver(targetRef, setIsSectionSeven, 0.1);
 
-  return (
+  const { isPhone, isSmallPhone } = useSelector((state) => state.breakPoints);
+
+  const isParallax = isPhone || isSmallPhone;
+
+  return isParallax ? (
+    <section ref={targetRef} className="homeSectionSeven">
+      <Parallax speed={10}>
+        <div className="container">
+          <div className="content-box">
+            <LeftSideTextBox />
+            <RightSideTextBox />
+          </div>
+        </div>
+      </Parallax>
+    </section>
+  ) : (
     <section ref={targetRef} className="homeSectionSeven">
       <div className="container">
         <div className="content-box">
